7ca492daf86085ee05723195ffe674082c35e073
YenTechProjects <phangcy04@gmail.com>
2022-06-05 13:02:03 +0800

    Just adding nodejs and expressjs modules and nodejs code to set up main pages

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-console.log("Collaboration process");
-console.log("Github is ready!!");
+//Required modules
+const express = require('express');
+const app = express();
+const path = require('path');
+
+//Include folder needed
+app.use('/app', express.static(path.join(__dirname, 'app')));
+
+//Set up webpages
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'app', 'main_page.html'));
+})
+
+//Listen to port
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+})
