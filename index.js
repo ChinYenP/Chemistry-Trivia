b681931db0c5d1fac762b818e669ece416fb5b8a
YenTechProjects <phangcy04@gmail.com>
2022-06-06 22:14:35 +0800

    Added a lot of webpages

--- index.js
+++ index.js
@@ -3,13 +3,39 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+
+
 //Include folder needed
-app.use('/app', express.static(path.join(__dirname, 'app')));
+app.use('/webpage', express.static(path.join(__dirname, 'webpage')));
+
+
 
 //Set up webpages
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'app', 'main_page.html'));
+    res.sendFile(path.join(__dirname, 'webpage', 'app/main_page.html'));
 })
+
+app.get('/quiz', (req, res) => {
+    res.sendFile(path.join(__dirname, 'webpage', 'quiz/quiz.html'));
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'webpage', 'about/about.html'));
+})
+
+app.get('/account', (req, res) => {
+    res.sendFile(path.join(__dirname, 'webpage', 'account/account.html'));
+})
+
+app.get('/daily-challenge', (req, res) => {
+    res.sendFile(path.join(__dirname, 'webpage', 'daily-challenge/daily-challenge.html'));
+})
+
+app.get('/tournament', (req, res) => {
+    res.sendFile(path.join(__dirname, 'webpage', 'tournament/tournament.html'));
+})
+
+
 
 //Listen to port
 const PORT = 3000;
